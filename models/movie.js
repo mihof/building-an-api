958ce9bf38016b5f631076c2618b383ac7fccac0
Michael <michaelhofmann987@gmail.com>
2019-10-01 15:30:56 -0700

    The lab

---
+++ models/movie.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var reviewSchema = new Schema({
+  content: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+}, {
+  timestamps: true
+});
+
+var movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  releaseYear: {
+    type: Number,
+    default: function () {
+      return new Date().getFullYear();
+    }
+  }, mpaaRating: String,
+  nowShowing: { type: Boolean, default: false },
+  reviews: [reviewSchema],
+  cast: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Performer'
+  }]
+  // studio: {type: Schema.Types.ObjectId, ref: 'Studio'}
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
